a3b77b1368711ae2e339498a8733706d10497c21
joni <joni.jimenez@parcelpoint.com.au>
2022-12-09 21:00:21 +0800

    So far so good. H moves

--- day9/puzzle1.js
+++ day9/puzzle1.js
@@ -1,10 +1,20 @@
 const fs = require('fs')
 
 // just  pass a length and what to draw where...
-const printSimulation = (array2d) => {
+// draw: [{s: [1, 0]}, {T: [5, 0]}]
+const printSimulation = (length, draw) => {
+  let array2d = new Array(length).fill(null).map(() => new Array(length).fill('.'))
+  draw?.forEach(e => {
+    let [x, y] = Object.values(e)[0]
+    let value = Object.keys(e)[0]
+    array2d[x][y] = value
+  })
+
   array2d.forEach(row => {
     console.log(row.join(' '))
   })
+
+  console.log('')
 }
 
 const printMessage = (message) => {
@@ -12,20 +22,31 @@ const printMessage = (message) => {
 }
 
 // ################# Main ################## //
-const main = (data, length, center) => {
-  let array2d = new Array(length).fill(null).map(r => new Array(length).fill('.'))
+const main = (data, length, center) => {  
+  const prepareSTHAndPrint = (sTH) => {
+    let print = [{s: sTH.s}, {T: sTH.T}, {H: sTH.H}]
+    printSimulation(length, print)
+  }
+
+  let state = {}
+  const setState = (change) => {
+    state = {
+      ...state,
+      ...change
+    }
+  }
   
   // ======= Initial Value ======= //
   // make s, T and H start at the beginning
-  array2d[center[0]][center[1]] = 'H'
+  setState({s: [5, 5], T: [5,5], H: [5,5]})
   printMessage('Initial State')
-  printSimulation(array2d)
+  prepareSTHAndPrint(state)
 
   let moves = {
     R: [0,1],
-    L: [0. -1],
-    U: [1, 0],
-    D: [-1, 0]
+    L: [0, -1],
+    U: [-1, 0],
+    D: [1, 0]
   }
 
   let keepTrack = {
@@ -35,14 +56,26 @@ const main = (data, length, center) => {
   }
 
   for (let i = 0; i < data.length; ++i) {
-    let move = data[i].split(' ')
+    printMessage(data[i])
+    let move = data[i].split(' ') // [R, 1]
 
-    for (let j = 0; j < move[1]; ++i) {
+    let numMoves = parseInt(move[1])
+    for (let j = 0; j < numMoves; ++j) {
+      // move the head
+      let direction = moves[move[0]]
+      let x = state.H[0] + direction[0]
+      let y = state.H[1] + direction[1]
 
+      setState({H:  [x, y]})
+      prepareSTHAndPrint(state)
     }
     
   }
 }
+
+
+
+
 
 // ################# Unit Testing ################## //
 // Given array of commands, simulate and check if right
@@ -55,4 +88,4 @@ const unitTesting = () => {
 
 // const input = fs.readFileSync('./day9/given.txt', 'utf-8')
 // const data = input.split('\n')
-main(['R 1'], 11, [5, 5])
+main(['R 1', 'U 2', 'L 1', 'R 2'], 11, [5, 5])
